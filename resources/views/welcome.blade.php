a9bf0838f04d4c27b35ca78b53d7a054ad0de4de
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-12-09 10:11:56 +0700

    add layouts and partials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,34 +1,16 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-    <title>Createve</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bilbo+Swash+Caps&family=Montserrat:wght@100;400;500;700&display=swap" rel="stylesheet">
-</head>
-<body class="h-[2000px]">
-    <nav class="bg-transparent text-white flex justify-between px-2 py-5 fixed top-0 left-0 right-0 z-10">
-        <a href="" class="font-montserrrat font-medium text-2xl">Createve</a>
+@extends('layouts.main')
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-        </svg>
-    </nav>
-
+@section('content')
     {{-- === JUMBOTRON === --}}
     <section class="w-full h-fit min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
         <div class="flex flex-col justify-center space-y-7 px-2 relative h-screen">
             <span class="font-bilbo text-5xl">Creative Thinking</span>
-    
+
             <span class="font-montserrrat font-bold text-4xl">Strategic Thinking, Tangible Result for Business</span>
-    
+
             <p class="text-lg leading-relaxed">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita repellendus nobis eveniet, quaerat possimus dolor.</p>
-    
+
             <button class="w-fit px-10 py-4 font-bold rounded-full absolute bottom-5 bg-gradient-to-r from-teal-400 to-cyan-500">Explore Now</button>
         </div>
     </section>
-</body>
-</html>
+@endsection
