fcd938d026f9e980e748cc5a3f1bef6d99a876f4
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-12-09 10:05:10 +0700

    home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,20 +19,16 @@
     </nav>
 
     {{-- === JUMBOTRON === --}}
-    <section class="w-full h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white flex flex-col justify-center space-y-7 px-2 relative">
-
-        <span class="font-bilbo text-5xl">Creative Thinking</span>
-
-        <div class="flex flex-col font-montserrrat font-bold text-4xl">
-            <span>Strategic Thinking,</span>
-            <span>Tangible Result for</span>
-            <span>Business</span>
+    <section class="w-full h-fit min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
+        <div class="flex flex-col justify-center space-y-7 px-2 relative h-screen">
+            <span class="font-bilbo text-5xl">Creative Thinking</span>
+    
+            <span class="font-montserrrat font-bold text-4xl">Strategic Thinking, Tangible Result for Business</span>
+    
+            <p class="text-lg leading-relaxed">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita repellendus nobis eveniet, quaerat possimus dolor.</p>
+    
+            <button class="w-fit px-10 py-4 font-bold rounded-full absolute bottom-5 bg-gradient-to-r from-teal-400 to-cyan-500">Explore Now</button>
         </div>
-
-        <p class="text-lg leading-relaxed">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita repellendus nobis eveniet, quaerat possimus dolor.</p>
-
-        <button class="bg-red-500 w-fit px-10 py-5 rounded-full absolute bottom-5 bg-gradient-to-r from-teal-400 to-cyan-500">Explore Now</button>
-
     </section>
 </body>
 </html>
