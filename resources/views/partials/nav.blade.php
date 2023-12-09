12e230b901d39fb9627b93bb779d0483c03a17fd
SatrioUtomo13 <satrio.utomo130799@gmail.com>
2023-12-09 10:12:38 +0700

    partials

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="bg-transparent text-white flex justify-between px-2 py-5 fixed top-0 left-0 right-0 z-10">
+    <a href="" class="font-montserrrat font-medium text-2xl">Createve</a>
+
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+    </svg>
+</nav>
